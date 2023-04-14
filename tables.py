79e5fcde18298458badbcdd541c942c88f79790b
Shubhendu <shubhenduanupam@gmail.com>
2023-04-14 18:03:24 +0530

    Added Favicons

--- tables.py
+++ tables.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
-    email = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(500), nullable=False)
     todo_list = relationship('Todo', back_populates='user')
 
